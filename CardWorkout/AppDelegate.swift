5cbf67ce891901833a6fce5522f6f588d57303aa
Maciek <46852030+recostudio@users.noreply.github.com>
2022-10-25 21:27:34 +0200

    New branch clean headers

--- CardWorkout/AppDelegate.swift
+++ CardWorkout/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  CardWorkout
-//
-//  Created by Maciej Szostak on 17/10/2022.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- CardWorkout/SceneDelegate.swift
+++ CardWorkout/SceneDelegate.swift
@@ -1,16 +1,8 @@
-//
-//  SceneDelegate.swift
-//  CardWorkout
-//
-//  Created by Maciej Szostak on 17/10/2022.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
