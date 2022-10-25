3ccf9fe5e6f14d659499c5572c32ef83365a2cb3
Maciek <46852030+recostudio@users.noreply.github.com>
2022-10-25 21:05:35 +0200

    ddddd

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -11,6 +11,10 @@ class CardSelectionVC: UIViewController {
 
     @IBOutlet var cardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
+<<<<<<< HEAD
+=======
+    var cards: [UIImage] = Card.allValues
+>>>>>>> CWStoryboardsFinished
     
     var timer: Timer!
     
@@ -23,16 +27,29 @@ class CardSelectionVC: UIViewController {
         }
     }
     
+<<<<<<< HEAD
+=======
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+>>>>>>> CWStoryboardsFinished
     func startTimer() {
         timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
     }
     
     @objc func showRandomImage() {
+<<<<<<< HEAD
+=======
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+>>>>>>> CWStoryboardsFinished
         print("Timer fired off after 0.5second")
         
     }
     
     @IBAction func stopButtonTapped(_ sender: Any) {
+<<<<<<< HEAD
     
     }
     
@@ -42,6 +59,18 @@ class CardSelectionVC: UIViewController {
     
     @IBAction func rulesButtonTapped(_ sender: Any) {
         
+=======
+        timer.invalidate()
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        startTimer()
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: Any) {
+//        i dont use it because i've used segue present screen modaly , i can delete it but left for explain
+>>>>>>> CWStoryboardsFinished
     }
     
 }
